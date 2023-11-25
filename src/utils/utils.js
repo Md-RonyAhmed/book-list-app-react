088ccbe0ff62684fd278b1b686e93d44df3ba2f0
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2023-11-25 11:21:21 +0600

    [DONE] updated utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,5 @@
 export const getDataFromLocalStorage = () => {
+
   const data = localStorage.getItem("books");
-  if (data) {
-    return JSON.parse(data);
-  }
-  return [];
+  return JSON.parse(data) || []
 };
